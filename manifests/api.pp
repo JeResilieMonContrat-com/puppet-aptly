9fdd595d6c8b6c4f56bf26a443167aa4bd090f23
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2024-08-08 13:03:04 +0200

    Get rid of legacy validate_* functions

--- manifests/api.pp
+++ manifests/api.pp
@@ -30,15 +30,12 @@
 #
 class aptly::api (
   Enum['stopped','running'] $ensure = running,
-  $user                             = 'root',
-  $group                            = 'root',
-  $listen                           = ':8080',
+  String $user                             = 'root',
+  String $group                            = 'root',
+  String $listen                           = ':8080',
   Enum['none','log'] $log           = 'none',
   Boolean $enable_cli_and_http      = false,
   ) {
-  validate_string($user, $group)
-  validate_re($listen, ['^([0-9.]*:[0-9]+$|unix:)'], 'Valid values for $listen: :port, <ip>:<port>, unix:///path')
-
   if $::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease, '15.04') < 0 {
     file { 'aptly-upstart':
       path    => '/etc/init/aptly-api.conf',
